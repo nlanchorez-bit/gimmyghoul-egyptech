d4d4e8eeb22f45425116c7beb3b3baedb6fe935d
nikonikoniii <nlanchorez@fit.edu.ph>
2025-11-25 11:45:50 +0800

    feat(Routes): Updated Routing & Controllers

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/login', 'Home::login');
+$routes->get('/signup', 'Home::signup');
+$routes->get('/moodboard', 'Home::moodboard');
+$routes->get('/roadmap', 'Home::roadmap');
