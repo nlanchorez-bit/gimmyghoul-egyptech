53da9368fcba60d4ab385c6ffe0ebd6165e5d7e5
Miro <gabriel.miro.velasco03@gmail.com>
2025-11-25 21:10:20 +0800

    feat(auth): Added Auth Controller with following functions and expose Routes
    - Added Entities and Model
    - Login = Auth::login
    - Logout = Auth::logout
    - Sign Up = Auth::signup

--- backend/app/Entities/User.php
+++ backend/app/Entities/User.php
@@ -6,7 +6,12 @@ use CodeIgniter\Entity\Entity;
 
 class User extends Entity
 {
-    protected $datamap = [];
-    protected $dates   = ['created_at', 'updated_at', 'deleted_at'];
-    protected $casts   = [];
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    protected $casts = [
+        'id'              => 'integer',
+        'account_status'  => 'integer',
+        'email_activated' => 'integer',
+        'newsletter'      => 'integer',
+    ];
 }
